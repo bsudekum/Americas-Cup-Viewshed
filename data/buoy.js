79c0a04195e998a0123b89fec90f74deffdf6bd6
Bobby Sudekum <rsudekum@gmail.com>
2012-06-10 21:02:02 -0700

    First commit

---
+++ data/buoy.js
@@ -0,0 +1,24 @@
+var buoy = {
+"type": "FeatureCollection",
+"features": [
+{ "type": "Feature", "id": 0, "properties": { "Id": 0, "part": "Buoy" }, "geometry": { "type": "Point", "coordinates": [ -122.450993, 37.811977 ] } }
+,
+{ "type": "Feature", "id": 1, "properties": { "Id": 0, "part": "Buoy" }, "geometry": { "type": "Point", "coordinates": [ -122.451991, 37.815432 ] } }
+,
+{ "type": "Feature", "id": 2, "properties": { "Id": 0, "part": "Buoy" }, "geometry": { "type": "Point", "coordinates": [ -122.411196, 37.817242 ] } }
+,
+{ "type": "Feature", "id": 3, "properties": { "Id": 0, "part": "Buoy" }, "geometry": { "type": "Point", "coordinates": [ -122.411938, 37.820802 ] } }
+
+]
+}
+
+var boat = {
+    "type": "Feature",
+    "properties": {
+        "popupContent": "Boat"
+    },
+    "geometry": {
+        "type": "Point",
+        "coordinates": [-122.42416,37.81428]
+    }
+};
---
+++ data/photo.js
@@ -0,0 +1,20 @@
+var photo = {
+"type": "FeatureCollection",
+                                                                                
+"features": [
+{ "type": "Feature", "id": 0, "properties": { "id": 1, "photo": "http:\/\/visuallybs.com\/maps\/viewshed\/photo\/green_scott.jpg", "title": "Green and Scott", "cansee": "yes" }, "geometry": { "type": "Point", "coordinates": [ -122.44024515151978, 37.79552540545307 ] } }
+,
+{ "type": "Feature", "id": 1, "properties": { "id": 2, "photo": "http:\/\/visuallybs.com\/maps\/viewshed\/photo\/green_brod.jpg", "title": "Green and Broderick", "cansee": "yes" }, "geometry": { "type": "Point", "coordinates": [ -122.44359254837036, 37.7951184623911 ] } }
+,
+{ "type": "Feature", "id": 2, "properties": { "id": 3, "photo": "http:\/\/visuallybs.com\/maps\/viewshed\/photo\/valejo.jpg", "title": "Vallejo and Laguna", "cansee": "yes" }, "geometry": { "type": "Point", "coordinates": [ -122.4301815032959, 37.795864522959086  ] } }
+,
+{ "type": "Feature", "id": 3, "properties": { "id": 4, "photo": "http:\/\/visuallybs.com\/maps\/viewshed\/photo\/val_fil.jpg", "title": "Vallejo and Fillmore", "cansee": "yes" }, "geometry": { "type": "Point", "coordinates": [ -122.43513822555542, 37.795237154349074  ] } }
+,
+{ "type": "Feature", "id": 4, "properties": { "id": 5, "photo": "http:\/\/visuallybs.com\/maps\/viewshed\/photo\/lyon_union.jpg", "title": "Lyon and Union", "cansee": "Yes" }, "geometry": { "type": "Point", "coordinates": [ -122.44687557220459, 37.795678008523424 ] } }
+,
+{ "type": "Feature", "id": 5, "properties": { "id": 6, "photo": "http:\/\/visuallybs.com\/maps\/viewshed\/photo\/green_scott.jpg", "title": "Green and Fillmore", "cansee": "yes" }, "geometry": { "type": "Point", "coordinates": [ -122.43533134460449, 37.79616972738355 ] } }
+,
+{ "type": "Feature", "id": 6, "properties": { "id": 7, "photo": "http:\/\/visuallybs.com\/maps\/viewshed\/photo\/buch_val.jpg", "title": "Buchanan and Vallejo", "cansee": "yes" }, "geometry": { "type": "Point", "coordinates": [ -122.43183374404907, 37.79566105264228 ] } }
+
+]
+}
